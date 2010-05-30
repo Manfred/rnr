480b335cfd42fbbcc40a87ffb04c6a75515ca4a6
Manfred Stienstra <manfred@fngtps.com>
2010-05-30 17:25:30 +0200

    Add shortcuts to the HTTP class for all regular HTTP methods.

--- lib/net/http/methods.rb
+++ lib/net/http/methods.rb
@@ -1,9 +1,24 @@
 module Net
   class HTTP
     module Methods
-      def get(url=nil, headers={}, body=nil, options={})
-        request = Net::HTTP::Request.new(:get, url, headers, body, options)
-        request.perform
+      def get(url, headers={}, options={})
+        Net::HTTP::Request.new(:get, url, headers, nil, options).perform
+      end
+      
+      def head(url, headers={}, options={})
+        Net::HTTP::Request.new(:head, url, headers, nil, options).perform
+      end
+      
+      def post(url, headers={}, body=nil, options={})
+        Net::HTTP::Request.new(:post, url, headers, body, options).perform
+      end
+      
+      def put(url, headers={}, body=nil, options={})
+        Net::HTTP::Request.new(:put, url, headers, body, options).perform
+      end
+      
+      def delete(url, headers={}, options={})
+        Net::HTTP::Request.new(:delete, url, headers, nil, options).perform
       end
     end
   end
--- test/functional/methods_test.rb
+++ test/functional/methods_test.rb
@@ -2,17 +2,43 @@ require File.expand_path('../../start', __FILE__)
 require 'uri'
 
 class MethodsTest < Net::HTTP::TestCase
-  test "has a GET shortcut" do
+  def setup
     response = Net::HTTP::Response.new
     response.status_code = 200
     response.reason_phrase = 'OK'
-    
     FakeSocket.chunks = [response.to_s]
-    
-    # We're stubbing the socket so the requested URI doesn't matter
-    response = Net::HTTP.get(URI.parse('http://www.example.com/resource'))
-    
-    assert_equal 200, response.status_code
-    assert_equal 'OK', response.reason_phrase
+  end
+  
+  test "has a GET shortcut" do
+    @response = Net::HTTP.get(URI.parse('http://www.example.com/resource'))
+    run_assertions
+  end
+  
+  test "has a HEAD shortcut" do
+    @response = Net::HTTP.head(URI.parse('http://www.example.com/resource'))
+    run_assertions
+  end
+  
+  test "has a POST shortcut" do
+    @response = Net::HTTP.post(URI.parse('http://www.example.com/resource'))
+    run_assertions
+  end
+  
+  test "has a PUT shortcut" do
+    @response = Net::HTTP.put(URI.parse('http://www.example.com/resource'))
+    run_assertions
+  end
+  
+  test "has a DELETE shortcut" do
+    @response = Net::HTTP.delete(URI.parse('http://www.example.com/resource'))
+    run_assertions
+  end
+  
+  
+  private
+  
+  def run_assertions
+    assert_equal 200, @response.status_code
+    assert_equal 'OK', @response.reason_phrase
   end
 end
